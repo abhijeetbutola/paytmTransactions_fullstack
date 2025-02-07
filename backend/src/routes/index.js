a8297450b94b5dff7224cdd49310789e055bf868
Abhijeet Butola <abhijeet.butola@gmail.com>
2025-02-07 11:24:50 +0530

    checkpoint

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import { userRouter } from "./user.js";
 
-export const router = express.Router();
+const router = express.Router();
 
+router.use("/user", userRouter)
+
+export { router as mainRouter };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
-import { router } from "./routes/index.js";
+import { mainRouter } from "./routes/index.js";
 
 dotenv.config();
 const app = express();
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/v1/", router);
+app.use("/api/v1/", mainRouter);
 
 // sample api route
 app.get("/", (req, res) => {
